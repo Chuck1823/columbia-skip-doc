7a001e93a024bb1a27c2c9fc0a6b136887812649
Charles Antoine Malenfant <charles_m.18@hotmail.com>
2023-03-08 23:18:16 -0500

    Added data pipeline func to format to OpenPrompt for input and dataset splitting, created first manual template and MLM tokenizer

--- src/columbia_skip_doc/backend.py
+++ src/columbia_skip_doc/backend.py
@@ -8,6 +8,7 @@ from utils import setup_logging
 from constants import PRJ_ROOT_STR
 
 from openprompt.plms import load_plm
+from openprompt.prompts import ManualTemplate
 
 FILE_NAME = __name__
 
@@ -16,24 +17,44 @@ _logger = setup_logging(logging.DEBUG, FILE_NAME)
 
 class OpenPromptPipeline:
     def __init__(self, model_type, model_name):
+        """ Constructor for OpenPromptPipeline class
+
+        Args:
+            model_type (str): the model type - see OpenPrompt documentation for supported models in openpromt.plms.__init__.py
+            model_name (str): the model name as seen on huggingface.co's model page
+        """
         self.model_type = model_type
         self.model_name = model_name
 
     def load_model(self):
+        """ Uses OpenPrompt function to load a pretrained model, the model's tokenizer, the model's config from huggingface,
+        and OpenPrompt's wrapper class object for the model's objective.
+
+        Returns:
+            plm: huggingface pretrained model
+            tokenizer: huggingface tokenizer for the model
+            model_config: huggingface model config
+            wrapper_class: OpenPrompt wrapper class for the model's objective
+        """
         return load_plm(self.model_type, self.model_name)
 
 
 
 if __name__ == "__main__":
     data_fp = os.path.join(PRJ_ROOT_STR, "data", "All-2479-Answers-retrieved-from-MedQuAD.csv")
-    batch_size = 32
     data_classes_fp = os.path.join(PRJ_ROOT_STR, "data", "All-qrels_LiveQAMed2017-TestQuestions_2479_Judged-Answers.txt")
-    shuffle = False
-    class_filter = None
 
-    dp = data_pipeline.DataPipeline(data_fp, batch_size, data_classes_fp, shuffle, class_filter)
-    dp.read_and_clean_data()
+    dp = data_pipeline.DataPipeline(data_fp, data_classes_fp)
+    data_df = dp.read_and_clean_data(id_col_data='AnswerID', id_col_data_classes='id')
+    data_dict = dp.split_data_into_dictionary(data_df)
 
     op = OpenPromptPipeline('bert', 'emilyalsentzer/Bio_ClinicalBERT')
     plm, tokenizer, model_config, WrapperClass = op.load_model()
     _logger.info(f"Loaded model: {plm}")
+
+    template_text = 'Question: {"placeholder":"text_a"} Answer: {"mask"}.'
+    template = ManualTemplate(tokenizer=tokenizer, text=template_text)
+    _logger.info(f"Example wrapped in template: {template.wrap_one_example(data_dict['train'][0])}")
+
+    wrapped_mlm_tokenizer = WrapperClass(max_seq_length=128, decoder_max_length=3, tokenizer=tokenizer,truncate_method="head")
+    _logger.info(f"Initialized tokenizer: {wrapped_mlm_tokenizer}")
--- src/columbia_skip_doc/datapipeline.py
+++ src/columbia_skip_doc/datapipeline.py
@@ -5,49 +5,95 @@ import utils
 
 from utils import setup_logging
 from constants import PRJ_ROOT_STR
+from openprompt.data_utils import InputExample
+
+from sklearn.model_selection import train_test_split
 
 FILE_NAME = __name__
 
 _logger = setup_logging(logging.DEBUG, FILE_NAME)
 
 class DataPipeline:
-    def __init__(self, data_fp, batch_size, data_classes_fp=None, shuffle=True, class_filter=None):
+    def __init__(self, data_fp, data_classes_fp=None, train_split=0.7, validation_split=0.2, test_split=0.1):
         """Constructor for DataPipeline class
 
         Args:
-        data_fp (DataFrame): _description_
-            batch_size (int): _description_
-            data_classes_fp (str, optional): absolute file path to file containing labels - assumes space-delimited txt file with label, id as columns.
-            Defaults to None.
-            shuffle (bool, optional): boolean indicating if we want to shuffle the data. Defaults to True.
-            class_filter (List[str], optional): list containing which examples' labels to keep. Defaults to None (meaning no filtering).
+        data_fp (DataFrame): absolute file path to csv file containing data
+        data_classes_fp (str, optional): absolute file path to cvs file containing labels - assumes space-delimited txt file with label, 
+        id as unnamed columns. Defaults to None.
+        train_split (float, optional): percentage of data to use for training. Defaults to 0.7.
+        validation_split (float, optional): percentage of data to use for validation. Defaults to 0.2.
+        test_split (float, optional): percentage of data to use for testing. Defaults to 0.1.
         """
         self.data_fp = data_fp
-        self.data_df = None
         self.data_classes_fp = data_classes_fp
-        self.data_classes_df = None
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.n_batches = len(data_fp) // batch_size
-        self.class_filter = class_filter
+        self.train_split = train_split
+        self.validation_split = validation_split
+        self.test_split = test_split
 
-    def read_and_clean_data(self):
+    def read_and_clean_data(self, id_col_data=None, id_col_data_classes=None):
+        # TODO: add data cleaning functionality and make it generic for different datasets
+        """
+        Function to read in data from a csv file into a pandas DataFrame and it's corresponding labels from a txt file into
+        a pandas DataFrame.
+        """
         try:
-            self.data_df = pd.read_csv(self.data_fp)
-            _logger.info(f"Successfully read data file of shape: {self.data_df.shape}")
+            data_df = pd.read_csv(self.data_fp)
+            _logger.info(f"Successfully read data file of shape: {data_df.shape}")
         except OSError as e:
             _logger.error("Could not read data file: {}".format(e))
             raise
 
         if os.path.exists(self.data_classes_fp):
-            self.data_classes_df = pd.read_csv(self.data_classes_fp, sep= " ", names=['label', 'id'])
-            _logger.info(f"Successfully read data classes file of shape: {self.data_classes_df.shape}")
+            data_classes_df = pd.read_csv(self.data_classes_fp, sep= " ", names=['label', 'id'])
+            data_classes_df = data_classes_df.reset_index(drop=True)
+            _logger.info(f"Successfully read data classes file of shape: {data_classes_df.shape}")
+            _logger.info("Merging the two datasets on common id column..")
+            try:
+                return data_df.merge(data_classes_df, how='inner', left_on=id_col_data, right_on=id_col_data_classes).drop(columns=[id_col_data_classes])
+            except KeyError as e:
+                _logger.error("Could not merge data and data classes files: {}".format(e))
+                raise
 
-        if self.class_filter is not None:
-            for class_label in self.class_filter:
-                self.data_df = self.data_df[self.data_df['label'] == class_label]
+        return data_df
+    
+    def split_data_into_dictionary(self, data_df):
+        # TODO: maybe have text_b with any extraneous text?
+        """ Method to split data into dictionary of dataframes based on class
 
-        self.data_df = self.data_df.dropna()
+        Args:
+            data_df (DataFrame): DataFrame containing the data
+            data_classes_df (DataFrame, optional): DataFrame containing the data classes
+        Returns:
+            dict: dictionary of lists of InputExample objects with keys being 'train', 'validation' and 'test'
+        """
+        raw_data_dict = {}
+        data_dict = {}
 
-        return
+        train_df = train_test_split(data_df, test_size= (self.test_split + self.validation_split), train_size=self.train_split)[0]
+        validation_df = train_test_split(data_df, test_size= self.test_split, train_size=self.validation_split)[0]
+        test_df = train_test_split(data_df, test_size= self.test_split, train_size=self.validation_split)[1]
+
+        raw_data_dict['train'] = train_df
+        raw_data_dict['validation'] = validation_df
+        raw_data_dict['test'] = test_df
+
+        for split in ['train', 'validation', 'test']:
+            df = raw_data_dict[split]
+            data_dict[split] = []
+            for idx, row in df.iterrows():
+                try:
+                    question = df.loc[idx]['Answer'].split("Question: ")[1].split("URL: ")[0].rstrip()
+                    answer = df.loc[idx]['Answer'].split("Question: ")[1].split("URL: ")[1].split("Answer: ")[1].rstrip()
+                    meta = {'answer_confidence': row['label'].split("-")[1]}
+                    guid = row['AnswerID']
+                    data_dict[split].append(InputExample(guid=guid, text_a=question, tgt_text=answer, meta=meta))
+                except KeyError as e:
+                    _logger.error(f"Example mismatch between example classes file and examples file for Answer ID {row['id']}: {e}")
+                    raise
+                except IndexError as e:
+                    _logger.error(f"Could not parse example for Answer ID {row['id']}: {e}")
+                    raise
+                
+        return data_dict
     
